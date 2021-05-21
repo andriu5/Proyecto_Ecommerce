e66c1e4c305575834c4447fe159e0cf35cc5f1ca
andresalvearc <andres.alvear5@gmail.com>
2021-05-21 19:02:43 -0400

    agregando aplicacion de administrado y realizando la primera migragracion

--- apps/administrador/views.py
+++ apps/administrador/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from apps.logReg.models import User
+import bcrypt
+from time import gmtime, strftime, time, localtime
+from datetime import datetime
+from django.http import JsonResponse
 
 # Create your views here.
+def index(request):
+    if request.method == "GET":
+        return render(request, "admin/index.html")
