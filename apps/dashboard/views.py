ae5c7f67f64e0075f627a9028547a8d2905aaf77
andresalvearc <andres.alvear5@gmail.com>
2021-05-23 03:30:44 -0400

    Agregando todos los cambios antes de agregar Productos

--- apps/dashboard/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from apps.logReg.models import User
-
-# Create your views here.
-def admin_dashboard(request):
-    if request.method == "GET":
-        context={
-        }
-        return render(request, "master/dashboard_orders.html", context)
