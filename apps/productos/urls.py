565de35c5787733690187be7eada0c6d795ffc8f
andresalvearc <andres.alvear5@gmail.com>
2021-05-26 21:46:17 -0400

    CRUD para Producto soportado: actualmente se puede Agregar, editar, eliminar, y ver informacion de los productos de la tienda.

--- apps/productos/urls.py
+++ apps/productos/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('dashboard/add_prod/', views.admin_productos, name='admin_productos'),
     path('add_prod/', views.add_producto, name='add_prod'),
     path('<int:id>/', views.edit_producto, name='edit_prod'), #EDIT producto!
-    path('update/<int:producto>/', views.update_producto, name='update_prod'), #UPDATE producto!
+    path('update/<int:id>/', views.update_producto, name='update_prod'), #UPDATE producto!
     path('<int:id>/destroy/', views.delete_producto, name='delete_prod'), # DELETE producto!
-    path('uploadFile/', views.uploadFile),
+    path('<int:id>/info/', views.info_producto, name='info_prod'), # INFO producto!
 ]
--- apps/productos/views.py
+++ apps/productos/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 #from apps.logReg.models import User
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 #from .models import Producto, Orden, OrdenItem
 from .models import Producto
 from .forms import  formProducto
@@ -9,97 +10,12 @@ import bcrypt
 from django.core.paginator import EmptyPage, Paginator
 from django.core.files.storage import FileSystemStorage
 
-productos = [
-    {
-        "id":1,
-        "nombre" : "Computador Portatíl",
-        "cantidad_inventario" : 100,
-        "cantidad_vendido" : 1,
-        "imagen" : {"url":"/media/notebook_hp_14_cf2051la_intel_CoreI3_4gb_Ram.jpg"},
-    },
-    {
-        "id":2,
-        "nombre" : "Jeans Mujer",
-        "cantidad_inventario" : 100,
-        "cantidad_vendido" : 99,
-        "imagen" : {"url":"/media/jeans_mujer.jpg"},
-    },
-    {
-        "id":3,
-        "nombre" : "Pantalon Trekking",
-        "cantidad_inventario" : 100,
-        "cantidad_vendido" : 85,
-        "imagen" : {"url":"/media/pantalon_trekking.jpg"},
-    },
-    {
-        "id":4,
-        "nombre" : "Pantalon Trekking Kaki",
-        "cantidad_inventario" : 100,
-        "cantidad_vendido" : 85,
-        "imagen" : {"url":"/media/pantalon_kaki-2.jpg"},
-    },
-    {
-        "id":5,
-        "nombre" : "Polera Seleccion Chilena 2019",
-        "cantidad_inventario" : 1000,
-        "cantidad_vendido" : 850,
-        "imagen" : {"url":"/media/SELEC.-CHILENA-2019.jpg"},
-    },
-    {
-        "id":6,
-        "nombre" : "Polera cuello redondo Naranja",
-        "cantidad_inventario" : 1000,
-        "cantidad_vendido" : 850,
-        "imagen" : {"url":"/media/polera-cuello-redondo-naranja.jpg"},
-    },
-    {
-        "id":7,
-        "nombre" : "Computador Portatíl",
-        "cantidad_inventario" : 100,
-        "cantidad_vendido" : 1,
-        "imagen" : {"url":"/media/notebook_hp_14_cf2051la_intel_CoreI3_4gb_Ram.jpg"},
-    },
-    {
-        "id":8,
-        "nombre" : "Jeans Mujer",
-        "cantidad_inventario" : 100,
-        "cantidad_vendido" : 99,
-        "imagen" : {"url":"/media/jeans_mujer.jpg"},
-    },
-    {
-        "id":9,
-        "nombre" : "Pantalon Trekking",
-        "cantidad_inventario" : 100,
-        "cantidad_vendido" : 85,
-        "imagen" : {"url":"/media/pantalon_trekking.jpg"},
-    },
-    {
-        "id":10,
-        "nombre" : "Pantalon Trekking Kaki",
-        "cantidad_inventario" : 100,
-        "cantidad_vendido" : 85,
-        "imagen" : {"url":"/media/pantalon_kaki-2.jpg"},
-    },
-    {
-        "id":11,
-        "nombre" : "Polera Seleccion Chilena 2019",
-        "cantidad_inventario" : 1000,
-        "cantidad_vendido" : 850,
-        "imagen" : {"url":"/media/SELEC.-CHILENA-2019.jpg"},
-    },
-    {
-        "id":12,
-        "nombre" : "Polera cuello redondo Naranja",
-        "cantidad_inventario" : 1000,
-        "cantidad_vendido" : 850,
-        "imagen" : {"url":"/media/polera-cuello-redondo-naranja.jpg"},
-    },
-]
-
 # Create your views here.
+@login_required
 def index(request):
     if request.method == "GET":
         # Paginamos el dashboard de Productos cada 4 elementos usando Paginator
+        productos = Producto.objects.all()
         p = Paginator(productos, 4)
         page_num = request.GET.get('/productos/dashboard/<int:page>/', 1)
         page = p.page(page_num) # página desde donde comenzamos a mostrar, ósea, si colocamos 2 entregara el id=5 e id=6
@@ -112,10 +28,12 @@ def index(request):
         }
         return render(request, "master/listar_productos.html", context)
 
+@login_required
 def dashboard(request, page):
     #tener cuidado con la seguridad de la pagina!
     if 'admin_loggedin' in request.session and request.session['admin_loggedin'] == True:
         # Paginamos el dashboard de Ordenes cada 4 elementos usando Paginator
+        productos = Producto.objects.all()
         p = Paginator(productos, 4)
         page_num = request.GET.get('dashboard/orders/<int:page>/',page)
 
@@ -135,6 +53,7 @@ def dashboard(request, page):
             }
         return render(request, "master/listar_productos.html", context)
 
+@login_required
 def admin_productos(request):
     if 'admin_loggedin' in request.session and request.session['admin_loggedin'] == True:
         context = {
@@ -146,12 +65,10 @@ def admin_productos(request):
     request.session['admin_bad_loggedin'] = True
     return redirect('administrador:index')
 
+@login_required
 def add_producto(request):
     print('*'*100)
     print('Agregando un nuevo Producto...')
-    if 'admin' not in request.session.keys():
-        print('Cookie deleted by user! redirecting user for login...')
-        return redirect('administrador:index')
     if 'admin' in request.session.keys():
         #Validamos el formulario contra la base de datos
         print(request.POST)
@@ -167,11 +84,6 @@ def add_producto(request):
             #     return redirect('productos:admin_productos')
             # else:
             if request.method == "POST":
-                # archivoCargado=request.POST['imagen']
-                # adminID = request.session['admin_id']
-                # fileSS = FileSystemStorage("media/"+adminID)
-                # fileSS.save(archivoCargado.name, archivoCargado)
-                # print("Tamaño del Archivo:", archivoCargado)
                 nuevo_producto = Producto.objects.create(
                     nombre = request.POST['nombre'],
                     precio = int(request.POST['precio']),
@@ -185,22 +97,65 @@ def add_producto(request):
                 return redirect('productos:index')
             return redirect('administrador:index')
 
+@login_required
 def edit_producto(request, id):
     print('*'*100)
     print(f'Editando Producto ID: {id}...')
-    pass
+    context = {
+        "user": request.user,
+        "producto": Producto.objects.get(id=id),
+    }
+    return render(request, "master/edit_productos.html", context)
+
+@login_required
 def update_producto(request, id):
     print('*'*100)
     print(f'Actualizando Producto ID: {id}...')
-    pass
+    esteProducto = Producto.objects.get(id=id)
+    if 'admin' in request.session.keys():
+        #TODO: Validamos el formulario contra la base de datos
+        print(request.POST)
+        errors = Producto.objects.product_validation(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect(f'/productos/{id}/')
+        else:
+            if request.method == "POST":
+                if request.POST['nombre']:
+                    esteProducto.nombre = request.POST['nombre']
+                if request.POST['precio']:
+                    esteProducto.precio = int(request.POST['precio'])
+                if request.POST['categoria']:
+                    esteProducto.categoria = request.POST['categoria']
+                if request.POST['inventario']:
+                    esteProducto.inventario = int(request.POST['inventario'])
+                if request.POST['descripcion']:
+                    esteProducto.descripcion = request.POST['descripcion']
+                if request.FILES['imagen']:
+                    esteProducto.imagen = request.FILES['imagen']
+                esteProducto.uploaded_by = request.user
+                esteProducto.save()
+                print(f"Info: Producto correctamente editado en la base de datos!\n")
+                return redirect('productos:index')
+            return redirect('administrador:index')
 
-def delete_producto(request):
-    pass
+@login_required
+def delete_producto(request,id):
+    print('*'*100)
+    print(f'Removiendo producto ID: {id}...')
+    deleteThisProducto = Producto.objects.get(id=id)
+    if request.user.is_authenticated:
+        deleteThisProducto.delete()
+        return redirect('productos:index')
+    else:
+        return redirect('administrador:index')
 
-#no se si lo use, solo estoy siguiendo el ejemplo de David!
-def uploadFile(request):
-    if request.method == "POST":
-        userID = 2
-        nivel_usuario = 2
-        archivoCargado = request.FILES["archivo"]
-        pass
+@login_required
+def info_producto(request,id):
+    print('*'*100)
+    print(f'Información producto ID: {id}...')
+    context = {
+        "producto" : Producto.objects.get(id=id),
+    }
+    return render(request, "master/ficha_producto.html", context)
